04710bacbd7e3a1770f4490015f4ca11770f5021
Fuze <67111447+BloxSense@users.noreply.github.com>
2026-09-03 15:33:47 +0300

    otkat

--- 3xinstall.sh
+++ 3xinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eu
+set -u
 
 EXTENDED_SETUP=false
 CLIENT_INSTALL=false
@@ -101,12 +101,10 @@ remove_existing_xui() {
 
 
 sni_request() {
-    echo ""
     echo -e "${cyan}Выберите SNI (Enter — вариант 1):${plain}"
     echo "  1. ${DOMAINS[0]}"
     echo "  2. ${DOMAINS[1]}"
     echo "  3. свой вариант"
-    echo ""
     read -rp "Ваш выбор [1-3]: " sni_choice
     sni_choice=${sni_choice:-1}
 
@@ -128,18 +126,14 @@ sni_request() {
             ;;
     esac
 
-    echo ""
     echo -e "${green}SNI / DEST: ${BEST_DOMAIN}${plain}"
-    echo ""
 }
 
 
 client_check() {
-    echo ""
     echo -e "${cyan}Установка клиенту? (Enter — вариант 1):${plain}"
     echo "  1. Да"
     echo "  2. Личное"
-    echo ""
     read -rp "Ваш выбор [1-2]: " install_choice
     install_choice=${install_choice:-1}
 
@@ -163,9 +157,7 @@ prompt_port() {
         read -rp $'\033[0;33mВведите порт для панели (Enter для 8080): \033[0m' USER_PORT
         PORT=${USER_PORT:-8080}
     else
-        echo ""
         echo -e "${yellow}Порт панели по умолчанию: ${PORT}${plain}" >&3
-        echo ""
     fi
 
     echo -e "Лог установки: ${cyan}${LOG_FILE}${plain}" >&3
@@ -716,17 +708,15 @@ print_summary() {
 }
 
 print_client_summary() {
-    echo -e "\n\033[1;32m══════════════════════════════════════════════════\033[0m" >&3
-    echo -e "" >&3
+    echo "" >&3
     echo "VLESS Reality: \`${VLESS_LINK}\`" >&3
     echo "" >&3
     echo "Hysteria2: \`${HY2_LINK}\`" >&3
     echo "" >&3
     echo "Панель 3x-ui:" >&3
-    echo "" >&3
     echo "http://${SERVER_IP}:${PORT}/${CLEAN_PATH}" >&3
-    echo "Логин: ${USERNAME}" >&3
-    echo "Пароль: ${PASSWORD}" >&3
+    echo "${USERNAME}" >&3
+    echo "${PASSWORD}" >&3
     echo "" >&3
     echo "Подключение протестировали, всё работает." >&3
     echo "" >&3
@@ -734,11 +724,6 @@ print_client_summary() {
     echo "V2Box на мобильном устройстве." >&3
     echo "" >&3
     echo "В Throne нажимаете по пустому ПКМ и выбираете добавить профиль из буфера обмена, в V2BOX в конфигурациях нажимаете + импортировать из буфера обмена." >&3
-    echo -e "" >&3
-    echo -e "\n\033[1;32m══════════════════════════════════════════════════\033[0m" >&3
-    echo -e "" >&3
-    echo -e "Все данные сохранены в файл: \033[1;36m/root/3x-ui.txt\033[0m" >&3
-    echo -e "Для просмотра в будущем введите: \033[0;36mcat /root/3x-ui.txt\033[0m\n" >&3
 }
 
 save_summary_file() {
